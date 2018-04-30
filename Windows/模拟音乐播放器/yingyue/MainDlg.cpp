3fe5adef98fa8d98fa8b113e7bc8422e6b522f9b
xincai juan <jxcjxcjzx@sina.com>
2018-04-30 20:07:12 +0800

    NU
    NU

---
+++ Windows/模拟音乐播放器/yingyue/MainDlg.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include <windows.h>
+#include <windowsx.h>
+#include <mmsystem.h>
+#include "resource.h"
+#include "MainDlg.h"
+
+/*
+Template designed by RuPeng.com. Please visit http://www.rupeng.com for more information
+如鹏网（http://www.rupeng.com）大学生计算机学习社区，提供大量免费视频学习教程，提供个性化一对一学习指导
+*/
+BOOL WINAPI Main_Proc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch(uMsg)
+    {
+        HANDLE_MSG(hWnd, WM_INITDIALOG, Main_OnInitDialog);
+        HANDLE_MSG(hWnd, WM_COMMAND, Main_OnCommand);
+		HANDLE_MSG(hWnd,WM_CLOSE, Main_OnClose);
+    }
+
+    return FALSE;
+}
+
+BOOL Main_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
+{
+    return TRUE;
+}
+
+void Main_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+    switch(id)
+    {
+        case IDC_1:
+		{
+			TCHAR shortPath[MAX_PATH];
+			GetShortPathName("C:\\Documents and Settings\\Jiangxincheng\\桌面\\歌曲收集\\1.wma",shortPath,sizeof(shortPath));
+            TCHAR cmd[MAX_PATH+10];
+			wsprintf(cmd,"play %s",shortPath);
+			mciSendString(cmd,"",0,NULL);
+		}
+        break;
+        default:
+		break;
+    }
+}
+
+void Main_OnClose(HWND hwnd)
+{
+    EndDialog(hwnd, 0);
+}
